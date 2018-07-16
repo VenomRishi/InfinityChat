8e96e88db6d0a62c61c2760ac0904956c44e0dfa
rishikesh mhatre <mhatrerishikesh@gmail.com>
2018-07-17 03:30:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/acer/infinitychat/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.acer.infinitychat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getInstance().getCurrentUser();
+        if(currentUser==null){
+            Intent start_intent=new Intent(MainActivity.this,StartActivity.class);
+            startActivity(start_intent);
+
+        }
+    }
+
+}
