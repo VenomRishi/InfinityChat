1323f1fcc4488a36bdddad4d96377761e3cec0f6
rishikesh <mhatrerishikesh@gmail.com>
2018-07-17 18:24:47 +0530

    layout toolbar is added

--- app/src/main/java/com/example/acer/infinitychat/MainActivity.java
+++ app/src/main/java/com/example/acer/infinitychat/MainActivity.java
@@ -4,26 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mToolbar=(Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Infinity Chat");
     }
+
+
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getInstance().getCurrentUser();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser==null){
             Intent start_intent=new Intent(MainActivity.this,StartActivity.class);
             startActivity(start_intent);
-
+            finish();
         }
     }
 
